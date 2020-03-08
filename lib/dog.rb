22391c0ca3c84b6d8c6bb1668e082a7022001d3e
Frank  D'Angelo <08fdange@gmail.com>
2020-03-08 19:23:43 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,27 @@
 class Dog
-  # code goes here
+  attr_accessor :owner, :mood, :dogs
+  attr_reader :name 
+  
+  @@all = []
+  
+  def initialize(name, owner)
+    @name = name
+    @owner = owner
+    @mood = "nervous"
+    save
+  end
+  
+  def save
+    @@all << self
+  end
+  
+  def owner=(name)
+    self.owner = name
+    self.save
+  end
+  
+  def self.all
+    @@all
+  end
+  
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,56 @@
 class Owner
-  # code goes here
+  attr_accessor :cats, :dogs
+  attr_reader :name, :species
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @species = "human"
+    save
+    @o_cats = []
+    @o_dogs = []
+  end
+  
+  def save
+    @@all << self
+  end
+  
+  def say_species
+    "I am a #{self.species}."
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.count
+    self.all.count
+  end
+  
+  def self.reset_all
+    @@all.clear
+  end
+  
+  def cats 
+    Cat.all.select do |instance|
+      self == instance.owner
+    end
+  end
+  
+  def dogs 
+    Dog.all.select do |instance|
+      self == instance.owner
+    end
+  end
+  
+  def buy_cat(cat_name)
+    Cat.all.select do |instance|
+      if cat_name == instance.name
+        instance.owner = self
+      end
+    end
+  end
+      
+      
+    
 end
